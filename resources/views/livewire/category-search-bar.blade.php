fe1fc6268265d98e81e3c1735016f9551ff9f8a8
Andrea Spadavecchia <andrea.spadavecchia@heply.it>
2021-04-02 16:05:10 +0200

    clean: remove comments

--- resources/views/livewire/category-search-bar.blade.php
+++ resources/views/livewire/category-search-bar.blade.php
@@ -1,7 +1,6 @@
 <div>
     <form action="/" method="get">
         <div class="relative">
-            <!-- <input wire:model="query" type="text" id="category" name="category" class="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors" placeholder="Search by category..."> -->
             <input wire:model="query" type="text" id="category" name="category" class="form-input w-full" autocomplete="off" placeholder="Search by category...">
             <div class="absolute z-10 list-group bg-white w-full rounded-t-none shadow-lg">
               @if(!empty($categories))
@@ -10,8 +9,6 @@
                   @endforeach
               @endif
             </div>
-            <!-- <button type="submit" class="block w-7 h-7 text-center text-xl leading-0 absolute top-2 right-2 text-gray-400 focus:outline-none hover:text-gray-900 transition-colors"><i class="mdi mdi-magnify">go</i></button> -->
-
         </div>
     </form>
 </div>
